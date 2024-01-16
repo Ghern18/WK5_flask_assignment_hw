e3476e0a7a236c3ab6af57dcd70917076340c4c1
Gabriela Hernandez <gabbyh1896@gmail.com>
2024-01-15 18:14:39 -0700

    sglalchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 from flask_smorest import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 from Config import Config
 
 
@@ -7,6 +10,11 @@ app = Flask(__name__)
 app.config.from_object(Config)
 api = Api(app)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models.student_model import StudentModel
+from models.spell_model import SpellModel
 
 from resources.student import bp as student_bp
 api.register_blueprint(student_bp)
---
+++ models/student_model.py
@@ -0,0 +1,23 @@
+from app import db
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class StudentModel(db.Model):
+
+    __tablename__ = 'students'
+
+    student_id = db.Column(db.Integer, primary_key = True)
+    student = db.Column(db.String(50), nullable = False, unique = True)
+    email = db.Column(db.String(75), nullable = False, unique = True)
+    password_hash = db.Column(db.String(250), nullable = False)
+    first_name = db.Column(db.String(30))
+    last_name = db.Column(db.String(30))
+
+    def __repr(self):
+        return f'<Student: {self.student}>'
+    
+    def commit(self):
+        db.session.add(self)
+        db.session.commit()
+
+ 
